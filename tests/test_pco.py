f5058c6c7a321b9d979b011a7c1aa34cfe6181b1
Bill Deitrick <bill.deitrick@cwc.life>
2018-02-20 13:44:46 -0500

    Start high-level tests of basic functionality

--- tests/test_pco.py
+++ tests/test_pco.py
@@ -3,6 +3,7 @@
 from pypco import PCO
 from pypco.pco import PCOAuthConfig
 import pypco.endpoints as endpoints
+import pypco.models as models
 from tests import BasePCOVCRTestCase
 
 #pylint: disable=E1101
@@ -21,3 +22,24 @@ class TestPCO(BasePCOVCRTestCase):
         pco = PCO(token="test")
 
         self.assertIsInstance(pco.people, endpoints.people.PeopleEndpoint)
+
+    def test_basic_crud_function(self):
+        """Test basic CRUD functionality of the API."""
+
+        pco = self.pco
+
+        # Test a simple get request
+        result = pco.people.people.get("16555904")
+        self.assertIsInstance(result, models.people.Person)
+        self.assertEqual(result.first_name, 'Paul')
+        self.assertEqual(result.last_name, 'Revere')
+
+        # Test a simple get request with multiple results
+        results = pco.people.people.list(where={'last_name': 'Revere'})
+        for result in results:
+            self.assertIsInstance(result, models.people.Person)
+            self.assertEqual(result.last_name, 'Revere')
+
+        # TODO: Test updating an object
+        # TODO: Test creating an object
+        # TODO: Test deleting an object
