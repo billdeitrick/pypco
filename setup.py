e133308b1c65aeb7407feed208ef6e1686fd2556
Bill Deitrick <bill@deitrick.us>
2018-11-23 23:48:57 -0500

    Add Python 3.7 classifier

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Libraries'
     ]
 )
